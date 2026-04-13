15981c786c9f53a5ade28925ae303301b0fd7890
Leng-Menghan <han230666@gmail.com>
2026-04-13 10:31:43 +0700

    Navigation

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,42 +1,59 @@
 import 'package:flutter/material.dart';
-import 'ui/widgets/actions/custom_button.dart';
-import 'ui/widgets/display/active_pass.dart';
-import 'ui/widgets/display/booking_card.dart';
-import 'ui/widgets/display/custom_chip.dart';
-import 'ui/widgets/display/inactive_pass.dart';
+import 'ui/screens/map/widgets/map_content.dart';
+import 'ui/screens/pass/widgets/pass_content.dart';
+import 'ui/screens/profile/widgets/profile_content.dart';
+import 'ui/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 2;
+
+  final List<Widget> _pages = [PassContent(), MapContent(), ProfileContent()];
+
+  @override
   Widget build(BuildContext context) {
+ 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: appTheme,
       home: Scaffold(
-        body: Column(
-          children: [
-            ActivePass(passType: PassType.day, startDate: DateTime(2026, 4, 11),),
-            const SizedBox(height: 20),
-            CustomChip(name: "Station 442",),
-            const SizedBox(height: 20),
-            CustomButton(name: "Confirm Booking", onPress: () => {},),
-            const SizedBox(height: 20),
-            CustomButton(name: "Confirm Booking", onPress: () => {}, isBig: false,),
-            const SizedBox(height: 20),
-            InactivePass(),
-            const SizedBox(height: 20),
-            BookingCard(),
+        body: _pages[_currentIndex],
+    
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          selectedItemColor: AppColors.label,
+          items: [
+             BottomNavigationBarItem(
+              icon: Icon(Icons.subscriptions),
+              label: 'Pass',
+            ),
+             BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Map',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
-
-
 
 
